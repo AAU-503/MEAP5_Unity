0558f138a75b86b1d983085db1b579ca2f4c97c1
Romaris <raminta.stanulionyte@gmail.com>
2016-10-22 14:03:58 +0200

    collision
    Lava and Box detection
    debug message on collision

---
+++ TestRunner/Assets/2D Platformer Controller/Scripts/HazardCollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class HazardCollision : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collide){
+		if(collide.gameObject.name == "Tile_Lava") {
+			Debug.Log("Lava");
+		}
+		
+	}
+
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.name == "Cube"){
+			Debug.Log("BOX");
+		}
+	} 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+}
